b964ec51218b5586428f9ad3afedae1829cfc7b7
Fadynour <fadynour194@gmail.com>
2025-06-29 13:26:20 +0300

    Q1

--- ass4/Program.cs
+++ ass4/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
+            #region Q1
+            //Console.WriteLine("enter number ro check divide on 3 or 4 : ");
+            //  int number =int.Parse(Console.ReadLine());
+            //    if (number % 3 == 0 && number % 4 == 0) 
+            //          Console.WriteLine("yes");
+            //    else
+            //          Console.WriteLine("no");
+            #endregion
 
-          
+
         }
     }
 }
